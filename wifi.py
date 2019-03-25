fe425906dd39e21ef58016fc8879d58506118690
arunksoman <42168602+arunksoman@users.noreply.github.com>
2019-03-25 15:09:22 +0530

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -2,6 +2,7 @@
 ## command for finding data
 
 import subprocess
+
 output = subprocess.run('sudo iwlist wlan0 scan |egrep "Cell|ESSID|Signal|Rates"', shell=True, stdout=subprocess.PIPE, 
                         universal_newlines=True)
-print(output.stdout)
+print(output.stdout, file=open("output.txt", "a"))
