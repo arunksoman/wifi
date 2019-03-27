412b1da2a06cebac2e3aa22c800c528e7a3794b4
arunksoman <42168602+arunksoman@users.noreply.github.com>
2019-03-27 08:24:41 +0530

    Create date_and_time.py

---
+++ date_and_time.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+print("----------------------------", file=open("output.txt", "a"))
+print(datetime.now(), file=open("output.txt", "a"))
+print("----------------------------", file=open("output.txt", "a"))
